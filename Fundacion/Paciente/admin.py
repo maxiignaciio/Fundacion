37e2062abfd0f1ac318bdb6e06b3042b66f0c507
maxiignaciio <101838260+maxiignaciio@users.noreply.github.com>
2022-06-28 20:31:35 -0400

    Delete Fundacion directory

--- Fundacion/Paciente/admin.py
+++
@@ -1,7 +0,0 @@
-from re import A
-from django.contrib import admin
-from .models import Paciente
-
-# Register your models here.
-admin.site.register(Paciente)
-
--- Fundacion/m.bat
+++
@@ -1,4 +0,0 @@
-call python manage.py runserver
-
-
-
